ccfa41283812691929759f3278a762de633c55cf
fuzzymillipede <fuzzymillipede@gmail.com>
2011-06-02 20:23:59 -0700

    Edited Server/src/petriwars/types/Obstacle.java via GitHub

--- Server/src/petriwars/types/Obstacle.java
+++ Server/src/petriwars/types/Obstacle.java
@@ -4,34 +4,101 @@ import java.util.ArrayList;
 
 
 public class Obstacle {
-	private final byte OBSTACLE = 0x00;
-	private ArrayList<Square> squares;
-	private ArrayList<Square> corners;
-
-	public Obstacle(byte[][] map_raw, Square[][] map, int x, int y)
-	{
-		squares = new ArrayList<Square>();
-		corners = new ArrayList<Square>();
-		grow(map_raw, map, x, y);
-	}
-
-	public void grow(byte[][] map_raw, Square[][] map, int x, int y)
-	{
-		squares.add(map[y][x]);
-		map[y][x].addObstacle(this);
-		if (x < map[0].length - 1 && map_raw[y][x + 1] == OBSTACLE)
-			grow(map_raw, map, x + 1, y);
-
-
-		if (y < map.length - 1)
-		{
-			if (map_raw[y + 1][x] == OBSTACLE)
-				grow(map_raw, map, x, y + 1);
-			if (x > 0 && map_raw[y + 1][x - 1] == OBSTACLE)
-				grow(map_raw, map, x - 1, y + 1);
-			if (x < map[0].length - 1 && map_raw[y + 1][x + 1] == OBSTACLE)
-				grow(map_raw, map, x + 1, y + 1);
-		}
-
-	}
+    private final byte OBSTACLE = 0x00;
+    private ArrayList<Square> squares;
+    private ArrayList<Square> corners;
+    
+    public Obstacle(byte[][] map_raw, Square[][] map, int x, int y)
+    {
+        squares = new ArrayList<Square>();
+        corners = new ArrayList<Square>();
+        grow(map_raw, map, x, y);
+    }
+    
+    public void grow(byte[][] map_raw, Square[][] map, int x, int y)
+    {
+        squares.add(map[y][x]);
+        map[y][x].addObstacle(this);
+        int w = map[0].length;
+        int h = map.length;
+        int[] consec = new int[4]; //0: x - 1, 1: x + 1, 2: y - 1, 3: y + 1
+        boolean[] dir = new boolean[2]; //0: up, 1: left
+        if (isObstacle(x - 1, y - 1, w, h, map_raw, map))
+        {
+            consec[0]++;
+            consec[2]++;
+        }
+        if (dir[0] = isObstacle(x, y - 1, w, h, map_raw, map))
+        {
+            consec[0]++;
+            consec[1]++;
+            consec[2]++;            
+        }
+        if (isObstacle(x + 1, y - 1, w, h, map_raw, map))
+        {
+            consec[1]++;
+            consec[2]++;
+        }
+        
+        if (dir[1] = isObstacle(x - 1, y, w, h, map_raw, map))
+        {
+            consec[0]++;
+            consec[2]++;
+            consec[3]++;
+        }
+        if (isObstacle(x + 1, y, w, h, map_raw, map))
+        {
+            consec[1]++;
+            consec[2]++;
+            consec[3]++;
+        }
+        
+        if (isObstacle(x - 1, y + 1, w, h, map_raw, map))
+        {
+            consec[0]++;
+            consec[3]++;
+        }
+        if (isObstacle(x, y + 1, w, h, map_raw, map))
+        {
+            consec[0]++;
+            consec[1]++;
+            consec[3]++;
+        }
+        if (isObstacle(x + 1, y + 1, w, h, map_raw, map))
+        {
+            consec[1]++;
+            consec[3]++;
+        }        
+           
+        if (consec[0] == 4)
+        {
+            if (dir[0]); //top left
+            else; //bottom left
+        }
+        if (consec[1] == 4)
+        {
+            if (dir[0]); //top right
+            else; //bottom right
+        }
+        if (consec[2] == 4)
+        {
+            if (dir[1]); //top left
+            else; //top right
+        }
+        if (consec[3] == 4)
+        {
+            if (dir[1]); //bottom left
+            else; //bottom right
+        }       
+    }
+    
+    private boolean isObstacle(int x, int y, int w, int h, byte[][] m, Square[][] m2)
+    {
+        if (x > 0 && y > 0 && x < w && y < h && m[y][x] == OBSTACLE)
+        {
+            grow(m, m2, x, y);
+            return true;
+        }
+        return false;
+    }
 }
